2fd2eb4ef5f9c15c49cd8cd8bafe5ad57e9d859d
PeiYumeng <457492875@qq.com>
2020-04-01 16:23:21 +0800

    modify

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ $(function(){
     var $box = $('#box');
     $ban.show({
         container:'#box',
-        num:50,
+        num:1000,
         img:["img/b1.png","img/b2.png","img/b3.png","img/b4.png","img/b5.png"],
         page:5
     })
